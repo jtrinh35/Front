2a01b166f976a9dd15e6d93799bfdfdc2ed5b99a
Joe Trinh <joe.trinh35@gmail.com>
2022-05-19 22:37:41 +0200

    ok

--- productModel.js
+++ productModel.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const productSchema = new mongoose.Schema({
-    name: {type: String, required: true, unique: false},
+    name: {type: String, required: true, unique: true},
     category: {type: String, required: false},
     image: {type: String, required: true},
     price: {type:Number, required: true},

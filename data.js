2fd32120259d6120e64157d62774cdc7ca5eef05
Joe Trinh <joe.trinh35@gmail.com>
2022-05-19 22:39:58 +0200

    ok

--- data.js
+++ data.js
@@ -1,31 +1,98 @@
 const data = {
-  products: [      
-    {
-      name: "Vide",
-      Code_Barre: "9780201379624",
-      price: 5.99,
-      CountInStock: 1,
-      description: "Pain au lait, saumon fumé, fromage frais Madame Loik, crème fraiche d\'Isigny, ciboulette, jus de citron, zeste de citron vert et poivre",
-      image: "https://i.ibb.co/H2x9xWB/petit-saumon-01-1.jpg"
-    },
-    {
-      name: "Vide1",
-      Code_Barre: "9780201379623",
-      price: 5.99,
-      CountInStock: 1,
-      description: "Pain au lait, saumon fumé, fromage frais Madame Loik, crème fraiche d\'Isigny, ciboulette, jus de citron, zeste de citron vert et poivre",
-      image: "https://i.ibb.co/H2x9xWB/petit-saumon-01-1.jpg"
-    },
-    {
-      name: "Vide2",
-      Code_Barre: "9780201379625",
-      price: 5.99,
-      CountInStock: 1,
-      description: "Pain au lait, saumon fumé, fromage frais Madame Loik, crème fraiche d\'Isigny, ciboulette, jus de citron, zeste de citron vert et poivre",
-      image: "https://i.ibb.co/H2x9xWB/petit-saumon-01-1.jpg"
-    },
-        
-  ]
+  products: [
+      
+          {
+            name: "Veste noire adidas trop cool",
+            Code_Barre: 'ABC-abc-1234',
+            category: 'M',
+            price: 35.95,
+            CountInStock: 1,
+            description: 'Veste noir en laine sans manches',
+            image: 'https://ibb.co/NVNXQgh'
+          },
+          {
+            name: "Manteau Kenzo",
+            Code_Barre: "ABC-abc-2456",
+            category: "L",
+            price: 120.99,
+            CountInStock: 1,
+            description: "Manteau d'hiver Kenzo avec capuche en poils",
+            image: "https://ibb.co/ZmHPYjB"
+          },
+          {
+            name: "Veste Bershka",
+            Code_Barre: "ABC-abc-3456",
+            category: "L",
+            price: 75.9,
+            CountInStock: 1,
+            description: "Veste Noir x Artiste dragon serpent dans le dos",
+            image: "https://ibb.co/g7hkN6T"
+          },
+          {
+            name: "Veste Levis",
+            Code_Barre: "ABC-abc-4567",
+            category: "M",
+            price: 100,
+            CountInStock: 1,
+            description: "Veste Levis avec capuche en poils",
+            image: "https://ibb.co/PFXvjZN"
+          },
+          {
+            name: "Sac Eastpak",
+            Code_Barre: "ABC-abc-5678",
+            category: "M",
+            price: 20.01,
+            CountInStock: 1,
+            description: "Sac eastpack gris",
+            image: "https://ibb.co/1MSBgSK"
+          },
+          {
+            name: "Livre Start up",
+            Code_Barre: "ABC-def-1234",
+            category: "",
+            price: 19.99,
+            CountInStock: 1,
+            description: "Livre comment de jeunes ing�nieurs montent des start up",
+            image: "https://ibb.co/gPcxFFL"
+          },
+          {
+            name: "Gel hydro",
+            Code_Barre: "ABC-def-2345",
+            category: "",
+            price: 5.99,
+            CountInStock: 1,
+            description: "Gel 99,99% efficace",
+            image: "https://ibb.co/sqGDTVT"
+          },
+          {
+            name: "Pull Gris burberry",
+            Code_Barre: "ABC-def-3456",
+            category: "M",
+            price: 60,
+            CountInStock: 1,
+            description: "Pull burberry",
+            image: "https://ibb.co/sKFTxjD"
+          },
+          {
+            name: "Pull bordeaux",
+            Code_Barre: "ABC-def-4567",
+            category: "L",
+            price: 59.89,
+            CountInStock: 1,
+            description: "Pull H&m",
+            image: "https://ibb.co/Km5T8y3"
+          },
+          {
+            name: "Airpods",
+            Code_Barre: "ABC-def-5678",
+            category: "",
+            price: 179.99,
+            CountInStock: 1,
+            description: "Air pods 1 génération",
+            image: "https://ibb.co/WxqF6TH"
+          }
+      
+  ],
 };
 
 export default data;

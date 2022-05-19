04941c5dd0bc510944fa68008567c80ab1c70d60
Joe Trinh <joe.trinh35@gmail.com>
2022-05-19 22:15:46 +0200

    ok

--- data.js
+++ data.js
@@ -2,62 +2,62 @@ const data = {
   products: [
       
           {
-            name: "Vide",
+            name: 'Vide',
             Code_Barre: '9780201379624',
             price: 5.99,
             CountInStock: 1,
             description: 'Pain au lait, saumon fumé, fromage frais Madame Loik, crème fraiche d\'Isigny, ciboulette, jus de citron, zeste de citron vert et poivre',
             image: 'https://i.ibb.co/H2x9xWB/petit-saumon-01-1.jpg'
           },
-  {
-            name: "Vide",
+          {
+            name: 'Vide',
             Code_Barre: '4192200312505',
             price: 5.99,
             CountInStock: 1,
             description: 'Pain au lait, saumon fumé, fromage frais Madame Loik, crème fraiche d\'Isigny, ciboulette, jus de citron, zeste de citron vert et poivre',
             image: 'https://i.ibb.co/H2x9xWB/petit-saumon-01-1.jpg'
           },
-  {
-            name: "Vide",
+          {
+            name: 'Vide',
             Code_Barre: '4866098487712',
             price: 5.99,
             CountInStock: 1,
             description: 'Pain au lait, saumon fumé, fromage frais Madame Loik, crème fraiche d\'Isigny, ciboulette, jus de citron, zeste de citron vert et poivre',
             image: 'https://i.ibb.co/H2x9xWB/petit-saumon-01-1.jpg'
           },
-  {
-            name: "Vide",
+          {
+            name: 'Vide',
             Code_Barre: '0530671398194',
             price: 5.99,
             CountInStock: 1,
             description: 'Pain au lait, saumon fumé, fromage frais Madame Loik, crème fraiche d\'Isigny, ciboulette, jus de citron, zeste de citron vert et poivre',
             image: 'https://i.ibb.co/H2x9xWB/petit-saumon-01-1.jpg'
           },
-  {
-            name: "Vide",
+          {
+            name: 'Vide',
             Code_Barre: '4090535811840',
             price: 5.99,
             CountInStock: 1,
             description: 'Pain au lait, saumon fumé, fromage frais Madame Loik, crème fraiche d\'Isigny, ciboulette, jus de citron, zeste de citron vert et poivre',
             image: 'https://i.ibb.co/H2x9xWB/petit-saumon-01-1.jpg'
           },
-  {
-            name: "Vide",
+          {
+            name: 'Vide',
             Code_Barre: '8881546571030',
             price: 5.99,
             CountInStock: 1,
             description: 'Pain au lait, saumon fumé, fromage frais Madame Loik, crème fraiche d\'Isigny, ciboulette, jus de citron, zeste de citron vert et poivre',
             image: 'https://i.ibb.co/H2x9xWB/petit-saumon-01-1.jpg'
           },
-  {
-            name: "Vide",
+          {
+            name: 'Vide',
             Code_Barre: '0373780789398',
             price: 5.99,
             CountInStock: 1,
             description: 'Pain au lait, saumon fumé, fromage frais Madame Loik, crème fraiche d\'Isigny, ciboulette, jus de citron, zeste de citron vert et poivre',
             image: 'https://i.ibb.co/H2x9xWB/petit-saumon-01-1.jpg'
           },
-  {
+          {
             name: "Vide",
             Code_Barre: '2590525354279',
             price: 5.99,
@@ -65,7 +65,7 @@ const data = {
             description: 'Pain au lait, saumon fumé, fromage frais Madame Loik, crème fraiche d\'Isigny, ciboulette, jus de citron, zeste de citron vert et poivre',
             image: 'https://i.ibb.co/H2x9xWB/petit-saumon-01-1.jpg'
           },
-  {
+         {
             name: "Vide",
             Code_Barre: '5019540975919',
             price: 5.99,

581a1276d1a5240c08273b80639ef73169e27904
Joe Trinh <joe.trinh35@gmail.com>
2022-05-19 22:38:41 +0200

    ok

--- data.js
+++ data.js
@@ -16,6 +16,14 @@ const data = {
       description: "Pain au lait, saumon fumé, fromage frais Madame Loik, crème fraiche d\'Isigny, ciboulette, jus de citron, zeste de citron vert et poivre",
       image: "https://i.ibb.co/H2x9xWB/petit-saumon-01-1.jpg"
     },
+    {
+      name: "Vide2",
+      Code_Barre: "9780201379625",
+      price: 5.99,
+      CountInStock: 1,
+      description: "Pain au lait, saumon fumé, fromage frais Madame Loik, crème fraiche d\'Isigny, ciboulette, jus de citron, zeste de citron vert et poivre",
+      image: "https://i.ibb.co/H2x9xWB/petit-saumon-01-1.jpg"
+    },
         
   ]
 };

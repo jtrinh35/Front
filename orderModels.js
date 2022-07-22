5c1951736f8f6692301872854b6fd3cc25f02e9f
Joe Trinh <joetrinh@mbp-de-joe.home>
2022-07-22 12:42:06 +0200

    ok

--- orderModels.js
+++ orderModels.js
@@ -22,5 +22,5 @@ const orderSchema = new mongoose.Schema({
     }
     );
      
-    const Order = mongoose.model('test_orders', orderSchema);
+    const Order = mongoose.model('demo_orders', orderSchema);
     export default Order;

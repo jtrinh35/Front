2dcbaf95da293407cc6be66cbc69c93f6a9149e2
LaurenceWang <61631525+LaurenceWang@users.noreply.github.com>
2023-06-19 17:33:27 +0200

    demo

--- src/pages/ScanCheck.jsx
+++ src/pages/ScanCheck.jsx
@@ -30,7 +30,7 @@ const ScanCheck = () => {
         return new ScanditSDK.ScanSettings({
         enabledSymbologies: ["ean8", "ean13", "upca", "code128"],
         codeDuplicateFilter: 2500,
-        searchArea: { x: 0.025, y: 0.1, width: 0.95, height: 0.36 },
+        searchArea: { x: 0, y: 0, width: 1, height: 0.3 },
         maxNumberOfCodesPerFrame: 1,
         });
     };
@@ -38,7 +38,7 @@ const ScanCheck = () => {
         console.log(access)
         if (access) {
           return (
-            <div >
+            <div className="successScandit">
               <ScanditBarcodeScanner
                 licenseKey={process.env.REACT_APP_SCANDIT_KEY}
                 engineLocation="https://cdn.jsdelivr.net/npm/scandit-sdk@5.x/build"
@@ -46,7 +46,7 @@ const ScanCheck = () => {
                 preloadEngine={true}
                 accessCamera={true}
                 guiStyle={BarcodePicker.GuiStyle.VIEWFINDER}
-                viewFinderArea={{ x: 0.2, y: 0.01, width: 0.6, height: 0.1 }}
+                viewFinderArea={{ x: 0.2, y: 0.01, width: 0.6, height: 0.5 }}
                 onScan={(scanResult) => {
                   setCode(scanResult.barcodes[0].data);
                   console.log(scanResult.barcodes[0].data);
@@ -60,6 +60,7 @@ const ScanCheck = () => {
                 enableTorchToggle={false}
                 mirrorImage={true}
                 zoom={0}
+
               />
             </div>
          
@@ -91,14 +92,31 @@ const ScanCheck = () => {
             {loading ? (
                 <div id="loader" class="loader loader-default is-active" data-text="Vérification en cours"></div>
             ) : (
-                <div className='w-full h-full bg-white'>
-                <div className='h-1/2'>
-                    Dirigez vous vers la sortie
+                <div className='w-full h-full bg-white flex flex-col '>
+
+                <div className='h-1/2  flex flex-col  items-center px-8 justify-around py-12'>
+                  <div className='flex justify-center items-center gap-5'> <img
+                    src="/images/success.png"
+                    alt="logo_success"
+                    className="w-12 mb-3 h-auto"
+                  />
+
+                  <h3 className="text-center text-2xl">Payment Successful</h3> </div>
+                   
+                    <h2 className='text-4xl '>Dernière étape</h2>
+
+                    <div className='scan-instruction p-10 rounded-[22px] ' >
+                      <h2 className='text-3xl '> Scannez le code QR de sortie</h2>
+                      <p className="text-2xl" style={{ fontFamily: "intermedium" }}>Pour afficher le reçu</p>
+
+                      <p className="mt-5 text-2xl" style={{ fontFamily: "intermedium" }}>The exit QR code is <strong> in a yellow frame</strong> and <strong>it is located near the payment desks</strong>. Please speak to a member of staff if you require help.</p>
+                    </div>
                 </div>
+                <div  className='h-10 px-10'>
 
                     {scan()}
               
-
+                </div>
                 </div>
             )}          
         </>

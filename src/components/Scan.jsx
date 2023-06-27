be480ccc3f7fb646815f8eb23e7be6e841626ded
LaurenceWang <61631525+LaurenceWang@users.noreply.github.com>
2023-06-27 11:56:51 +0200

    clean scan.jsx

--- src/components/Scan.jsx
+++ src/components/Scan.jsx
@@ -51,8 +51,6 @@ const Scan = () => {
   const prevOnlineStatus = useRef(isOnline);
   const cart = useSelector(state => state.cart)
 
-  let findProduct;
-
   useEffect(() => {
        
     if(order){
@@ -89,25 +87,6 @@ const Scan = () => {
     };
   }, []);
 
-  
-
-  /*useEffect(()=>{
-
-    if(!isOnline){     
-     ToastInternet( "noInternet", "Aucune connexion internet");
-     
-    }else{
-
-      toast.dismiss();
-      ToastInternet("internet", "connexion rétablie");
-      setTimeout(() => {
-        toast.dismiss();
-      }, 1500)
-
-
-    }
-  
-  }, [isOnline]);*/
 
   const refreshScan = () => {
     // setProduct()
@@ -136,7 +115,6 @@ const Scan = () => {
     setOverflowStyle("overflow-hidden");
   };
 
-  
 
   const scan = () => {
     if (access) {
@@ -184,15 +162,13 @@ const Scan = () => {
   const addToCartHandler = () => {
     if (product) {
       dispatch(addToCart(order._id, product, qty, axiosInstance));
-      console.log("order items dans addcart")
-      console.log(order);
       setCode(0);
     }
     
     setLoading(false);
     setProduct(false);
 
-    const element = document.getElementById('footer-cart');
+    /*const element = document.getElementById('footer-cart');
     const rect = element.getBoundingClientRect();
     console.log("offseeeet")
     console.log(rect);
@@ -200,7 +176,7 @@ const Scan = () => {
     const dot = document.getElementById('cart-item');
     const dot_offset = dot.getBoundingClientRect();
     console.log("offset dot")
-    console.log(dot_offset)
+    console.log(dot_offset)*/
     
   };
 
@@ -266,7 +242,7 @@ const Scan = () => {
     <>
     {order ? <>
     <div className={overflowStyle} id="scan-main" >
-      <div className="scan-screen  ">
+      <div className="min-h-full min-w-full bg-black text-white ">
         {loading ? (
           <>
             <div className="loader loader-default is-active"></div>
@@ -276,28 +252,28 @@ const Scan = () => {
               
                {localStorage.getItem("scanner") ? <>      <Header />  {scan()}     
                <div className="absolute w-full h-fit text-center m-auto top-2/4">
-                  <div className="flex justify-center " id="information">
-                    <p className="information_text text-lg h-screen w-screen"  onClick={productPopup}>Scan the barcode of your products</p>     
+                  <div className="flex justify-center" id="">
+                    <p className="text-lg h-screen w-screen"  onClick={productPopup}>Scan the barcode of your products</p>     
                   </div>
                  
                 </div>
                 <span className="cart-item" id="cart-item"></span>
             { <FooterNavbar props={{scan:true}}/>}
 
-            </> : <><div id="scan" className="w-screen relative flex flex-col justify-start items-center" >
-              <div className="brand_scan bg-black z-20  relative py-8 flex justify-center items-center">
-              <img id="brand" className="w-10" src="/images/Logo_Pineapple.png" alt="logo" />
-                <img id="brand" className="w-40" src="https://i.ibb.co/LR2v5bR/Pikko-1.png" alt="logo" />
+            </> : <><div className="w-screen relative flex flex-col justify-start items-center" >
+              <div className="bg-black z-20 relative py-8 flex justify-center items-center">
+              <img className="w-10 h-auto" src="/images/Logo_Pineapple.png" alt="logo" />
+              <img className="w-40 h-auto" src="https://i.ibb.co/LR2v5bR/Pikko-1.png" alt="logo" />
               </div>
 
-            <div className=" brand_overlay absolute opacity-60 z-10 w-full bg-black opactiy-75" > </div>
+            <div className="brand_overlay absolute opacity-60 z-10 w-full bg-black opactiy-75" > </div>
              <Header /> 
               
 
-              <div className="scan-overlay rounded-t-[25px] w-screen px-4 bg-white n"  onClick={() => deployScan()}>
+              <div className="scan-overlay text-black rounded-t-[25px] w-screen px-4 bg-white n"  onClick={() => deployScan()}>
                 <div className="container flex flex-col items-center justify-start px-8 text-center mx-auto">
 
-                  <img id="brand" className="my-16 w-96" src="/images/scan_pic.png" alt="" />
+                  <img className="my-16 w-96" src="/images/scan_pic.png" alt="" />
 
                   <h1 className="mt-4 mb-8">Get Started With PikkoPay</h1>
 
@@ -316,16 +292,9 @@ const Scan = () => {
                   >
                     Let's go
                   </button>
-                  {/* {localStorage.getItem("scanner") ? <>{scan()}</> : <></>} */}
-
-                  {/* {scan()} */}
                 </div>
               </div>
             </div></>} 
-
-
-            
-              {/* {localStorage.getItem("scanner") ? <>{scan()}</> : <></>} */}
 
             {product ? (
               <>
@@ -333,45 +302,19 @@ const Scan = () => {
                   className="fixed bottom-0 z-20 w-screen"
                 
                 >
-
-                  
-                  {/* <div className="relative top-24 right-10">
-                    <img src="images/valid_mail.png" alt="valid" />
-                  </div>
-                  <div className="text-center mb-4 relative left-8">
-                    <span
-                      style={{
-                        color: "#85DE9D",
-                        fontFamily: "poppinssemibold",
-                        fontSize: "13px",
-                      }}
-                    >
-                      Le produit a été ajouté à ton panier
-                    </span>
-                  </div> */}
                   <div
-                    id="popup_product"
-                    className="bg-white py-4 px-8 flex flex-col justify-evenly items-center rounded-t-[16px] "
+                    className="bg-white text-black py-4 px-8 flex flex-col justify-evenly items-center rounded-t-[16px] "
                   
                   >
                     <div className="dashed flex justify-center items-center w-full pb-8"> 
-                    <div className="popupimage">
-                      <img src={product.image} className="h-24"></img>
+                    <div className="flex justify-center items-center">
+                      <img src={product.image} className="w-24 h-auto"></img>
                     </div>
-                    <div className="popupdescription py-4 ml-4  ">
-                      <div className="min-30 text-left Break Words  ">
+                    <div className="h-full flex flex-col justify-center items-center py-4 ml-4  ">
+                      <div className="min-30 text-left">
                         <p className='mb-1 w-60 text-2xl text-black' >{product.name}</p>
                         <p className='text-2xl text-black'><strong> {product.price}€ </strong></p>
                       </div>
-                      {/* <div
-                        className="min-30_price !text-black"
-                        style={{
-                          fontFamily: "poetsen_oneregular",
-                          fontSize: "25px",
-                        }}
-                      >
-                        {product.price}€
-                      </div> */}
                     </div>
                     {qty < 2 ? (
                       <div className="min-30_price rounded-full border-solid px-6 py-1 flex items-center text-2xl relative">
@@ -396,12 +339,6 @@ const Scan = () => {
                     <div className="flex w-full">
                       <button className="rounded-full mt-5 mb-4 py-6 justify-self-end pikko-btn w-full" onClick={() => addToCartHandler()}>Add to cart</button>
                     </div>
-
-                    {/* <div className="min-30_price rounded-full border-solid px-6 py-1 flex items-center text-2xl relative">{qty}
-                    <button className='top-1 border-none pl-4 relative top-px' onClick={() => incQty()}>+</button>
-                    </div> */}
-                    {/* <button onClick={() => addToCartHandler()}>ADD</button> */}
-                    
                   </div>
                 </div>
 
@@ -409,43 +346,13 @@ const Scan = () => {
               </>
             ) : (
               <>
-                <div className="bordures_scan" id="bordures_scan"></div>
-                <div
-                  className="justify-center"
-                  style={{
-                    height: "100vh",
-                    height: "-webkit-calc(100vh - 65px)",
-                    height: "-moz-calc(100vh - 65px)",
-                    height: "calc(100vh  - 65px)",
-                    display:"none"
-                  
-                  }}
-                >
-                  <div className="absolute w-full text-center m-auto top-2/4">
-                    {/* <div className="flex justify-center" id="information" onClick={info}> */}
-                    <div className="flex justify-center" id="information">
-                      {/* <img src="/images/formule.png" alt="info" id="information_img"/> */}
-                       <span className="information_text">Scan the barcode of your products</span> 
-                    </div>
-                    {/* <div className='w-full flex justify-center'>
-                                <div id="formule" className='w-9/12 rounded-3xl py-2 text-white' style={{visibility: "hidden"}}>
-                                    <div>
-                                        <img src="/images/barcode-white.png" alt="barcode" className="mx-auto h-16 w-auto"/>
-                                    </div>
-                                    <div className="text-3xl break-words pb-2" style={{fontFamily:"poppinssemibold"}}>Scanne le code barre</div>
-                                    <div className="text-2xl break-words pb-4" style={{fontFamily:"poppinsregular"}}>de tes produits</div>
-                                </div>
-                            </div> */}
-                  </div>
-                </div>
               </>
             )}
           </>
         )}
       </div>
-    </div>
-                            
-    </> :   <></> }
+    </div>                         
+    </> : <></> }
     </>
   );
   
